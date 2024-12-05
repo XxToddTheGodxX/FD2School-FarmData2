235b685636c477fe84d9a8f1aef4467ed5bfe986
HuuPhu-Nguyen <nguyehuu@dickinson.edu>
2024-12-05 11:25:53 -0500

    added test for the first part

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReportSummaryTable.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReportSummaryTable.spec.js
@@ -26,4 +26,22 @@ describe('Testing for the Tray Seeding Summary Table', () => {
         cy.get('[data-cy=tray-summary]').should('not.exist')
     })
 
+    it('Check if the Tray Seeding Summary Table exists and is visible when “All” is selected in the seeding type filter', () => {
+        cy.get('[data-cy=start-date-select]').type('2015-01-01')
+        cy.get('[data-cy=end-date-select]').type('2019-11-23')
+        cy.get('[data-cy=generate-rpt-btn]').click()
+        cy.get('[data-cy = seeding-type-dropdown] > [data-cy=dropdown-input]').select("All")
+        cy.get('[data-cy=tray-summary]').should('exist')
+        cy.get('[data-cy=tray-summary]').should('be.visible')
+    })
+
+    it('Check if the Tray Seeding Summary Table exists and is visible when “Tray Seedings” is selected in the seeding type filter', () => {
+        cy.get('[data-cy=start-date-select]').type('2015-01-01')
+        cy.get('[data-cy=end-date-select]').type('2019-11-23')
+        cy.get('[data-cy=generate-rpt-btn]').click()
+        cy.get('[data-cy = seeding-type-dropdown] > [data-cy=dropdown-input]').select("Tray Seedings")
+        cy.get('[data-cy=tray-summary]').should('exist')
+        cy.get('[data-cy=tray-summary]').should('be.visible')
+    })
+
 })
