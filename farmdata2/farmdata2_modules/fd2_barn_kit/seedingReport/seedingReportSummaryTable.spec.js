49adbc81cc4d728c424c5235dec62bfb9efd635b
HuuPhu-Nguyen <nguyehuu@dickinson.edu>
2024-12-05 11:44:59 -0500

    copied test from issue2 to Phu

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReportSummaryTable.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReportSummaryTable.spec.js
@@ -14,26 +14,17 @@ describe('Testing for the Tray Seeding Summary Table', () => {
         cy.get('[data-cy=generate-rpt-btn]').click()
 
         // cy.get('[data-cy=seeding-type-dropdown] > [data-cy=dropdown-input]')
-        cy.get('[data-cy=start-date-select]').type('2024-06-21')
-        cy.get('[data-cy=end-date-select]').type('2024-11-28')
-        cy.get('[data-cy=generate-rpt-btn]').click()
-
-        // cy.get('[data-cy=seeding-type-dropdown] > [data-cy=dropdown-input]')
         //.select('Tray Seedings') Maybe we need to find a date where there exist some seedings but not tray seedings?
         cy.get('[data-cy=no-logs-message]').should('be.visible').should('contain.text', 'No Logs Found in These Dates');
-        cy.get('[data-cy=no-logs-message]').should('be.visible').should('contain.text', 'No Logs Found in These Dates');
     })
-
 
     it('Tray Seeding Summary Table not visible when "Direct Seedings" is chosen', () => {
         cy.get('[data-cy=start-date-select]').type('2020-01-01')
         cy.get('[data-cy=end-date-select]').type('2020-11-23')
         cy.get('[data-cy=generate-rpt-btn]').click()
-        cy.get('[data-cy=generate-rpt-btn]').click()
         cy.get('[data-cy = seeding-type-dropdown] > [data-cy=dropdown-input]').select("Direct Seedings")
         cy.get('[data-cy=tray-summary]').should('not.exist')
     })
-
 
     it('Check if the Tray Seeding Summary Table exists and is visible when “All” is selected in the seeding type filter', () => {
         cy.get('[data-cy=start-date-select]').type('2015-01-01')
